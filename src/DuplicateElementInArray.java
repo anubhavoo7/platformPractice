6704951383d48d80516f4b3606d5d355f1875ce5
anubhavoo7 <anubhavmishra299@gmail.com>
2023-10-26 00:28:12 +0530

    Duplicate And romanToInteger

---
+++ src/DuplicateElementInArray.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class DuplicateElementInArray {
+
+    public boolean containsDuplicate(int[] nums){
+        HashMap<Integer,Integer> map=new HashMap();
+
+        for(int i=0;i<nums.length;i++){
+            map.putIfAbsent(nums[i],0);
+            map.put(nums[i],map.get(nums[i]+1));
+            if(map.get(nums[i])>1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
